65053bff8f71ee04c0f0762bf7a45e85a0d45fb3
Sergey Kulik <xcwgf666@gmail.com>
2018-09-27 19:26:39 +0300

    Adding basic endpoint in the chosen framework

---
+++ basic_endpoint/hello.cpp
@@ -0,0 +1,27 @@
+#include "crow.h"
+
+int main()
+{
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/hello")
+    ([]{
+        crow::json::wvalue responseJson;
+        responseJson["message"] = "Hello, World!";
+
+        crow::response response(responseJson);
+	/*
+        This is how we can set response headers. In this example, we'd had to set Content-Type header of the response.
+        But there's no need in that, because Crow sets this header by default, when we use JSON-based constructor.
+
+        response.add_header("Content-Type", "application/json");
+	*/
+
+        return response;
+    });
+
+    app.loglevel(crow::LogLevel::DEBUG);
+    app.port(18080)
+        .multithreaded()
+        .run();
+}
